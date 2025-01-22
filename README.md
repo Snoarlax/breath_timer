8ce7c99665121b5f2359eb96b46ebaead8c56e32
sn0arlax <snoarsec@gmail.com>
2025-01-22 00:00:00 +0000

    Added sound effect so you don't need to look at the tray

--- README.md
+++ README.md
@@ -6,6 +6,10 @@ A linux widget that will help time breaths to 6 bpm
 
 Alot of the initial code was from here: [http://candidtim.github.io/appindicator/2014/09/13/ubuntu-appindicator-step-by-step.html](http://candidtim.github.io/appindicator/2014/09/13/ubuntu-appindicator-step-by-step.html)
 
+Sound Effect by [https://pixabay.com/users/u\_hm3ohsiik6-46090833/?utm\_source=link-attribution&utm\_medium=referral&utm\_campaign=music&utm\_content=242641](u\_hm3ohsiik6) from [https://pixabay.com/sound-effects//?utm\_source=link-attribution&utm\_medium=referral&utm\_campaign=music&utm\_content=242641](Pixabay)
+## Requirements
+
 - sudo apt install libcairo2-dev libxt-dev libgirepository1.0-dev
 - sudo apt install gir1.2-appindicator3-0.1
 - pip3 install PyGObject pycairo  
+- pip install playsound
--- app.py
+++ app.py
@@ -7,15 +7,25 @@ gi.require_version('Gtk', '3.0')
 gi.require_version('AppIndicator3', '0.1')
 from gi.repository import Gtk as gtk
 from gi.repository import AppIndicator3 as appindicator
+from playsound import playsound
 
 APPINDICATOR_ID = 'breath_timer'
 INTERVAL = 10
 BREATH_DURATION = 4
 OUT_ICON=os.path.dirname(os.path.realpath(__file__))+"/exhale.svg"
 IN_ICON=os.path.dirname(os.path.realpath(__file__))+"/breathe.svg"
+SOUND_ENABLED = True
+
+def breathe_sound():
+    playsound_args = (os.path.dirname(os.path.realpath(__file__))+"/sound.mp3",)
+    sound_thread = threading.Thread(target=playsound, args=playsound_args)
+    sound_thread.daemon = True
+    sound_thread.start()
 
 def breathe_in():
     print("Breathe in")
+    if SOUND_ENABLED:
+        breathe_sound()
     indicator.set_icon(IN_ICON)
 
 def breathe_out():
@@ -33,11 +43,22 @@ def quit_app(_):
     print("Quitting...")
     gtk.main_quit()
 
+def toggle_sound(_):
+    global SOUND_ENABLED
+    SOUND_ENABLED = not SOUND_ENABLED
+    toggle_sound_menu.set_label("Sound Enabled" if SOUND_ENABLED else "Sound Disabled")
+    print(f'Toggle sound: Now {SOUND_ENABLED}')
+
 def get_menu():
     menu = gtk.Menu()
     quit_menu = gtk.MenuItem(label="Quit")
     quit_menu.connect('activate', quit_app)
     menu.append(quit_menu)
+
+    global toggle_sound_menu
+    toggle_sound_menu = gtk.MenuItem(label="Sound Enabled")
+    toggle_sound_menu.connect("activate", toggle_sound)
+    menu.append(toggle_sound_menu)
 
     menu.show_all()
     return menu
